8f699636aed65d4fecfb2b62f7e3538d50fc5f6d
David Miguel de la Fuente <dmd0019@alu.ubu.es>
2014-05-15 19:26:39 +0200

    Move method 'lanSimulation.Network.printDocument(...)' to 'lanSimulation.Network.printDocument().printer' - Original project: '1314dass02' - Original element: 'lanSimulation.Network.printDocument(...)' - Keep original element as deprecated delegate to refactored element - Moved method: N/A - Target element: 'lanSimulation.Network.printDocument().printer' - Method name: 'printDocument' - Parameter name: 'network'

--- src/lanSimulation/internals/Node.java
+++ src/lanSimulation/internals/Node.java
@@ -90,4 +90,48 @@ Construct a <em>Node</em> with given #type and #name, and which is linked to #ne
 		};
 	}
 
+	public boolean printDocument (Network network, Packet document, Writer report) {
+		String author = "Unknown";
+		String title = "Untitled";
+		int startPos = 0, endPos = 0;
+	
+		if (type_ == Node.PRINTER) {
+			try {
+				if (document.message_.startsWith("!PS")) {
+					startPos = document.message_.indexOf("author:");
+					if (startPos >= 0) {
+						endPos = document.message_.indexOf(".", startPos + 7);
+						if (endPos < 0) {endPos = document.message_.length();};
+						author = document.message_.substring(startPos + 7, endPos);};
+						startPos = document.message_.indexOf("title:");
+						if (startPos >= 0) {
+							endPos = document.message_.indexOf(".", startPos + 6);
+							if (endPos < 0) {endPos = document.message_.length();};
+							title = document.message_.substring(startPos + 6, endPos);};
+							network.printAccounting(report, author, title);
+							report.write(">>> Postscript job delivered.\n\n");
+							report.flush();
+				} else {
+					title = "ASCII DOCUMENT";
+					if (document.message_.length() >= 16) {
+						author = document.message_.substring(8, 16);};
+						network.printAccounting(report, author, title);
+						report.write(">>> ASCII Print job delivered.\n\n");
+						report.flush();
+				};
+			} catch (IOException exc) {
+				// just ignore
+			};
+			return true;
+		} else {
+			try {
+				report.write(">>> Destinition is not a printer, print job cancelled.\n\n");
+				report.flush();
+			} catch (IOException exc) {
+				// just ignore
+			};
+			return false;
+		}
+	}
+
 }
